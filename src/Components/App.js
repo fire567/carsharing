f2877ef61233dda1429f6cc496d507e5e393ebea
fire567 <dante.nero.95@gmail.com>
2021-04-15 23:09:04 +0300

    SideBar

---
+++ src/Components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="Sidebar">
+        <div className="menu-btn">
+          <div className="vector"></div>
+          <div className="vector"></div>
+          <div className="vector"></div>
+        </div>
+        <div className="ellipse">
+          <div className="language">
+            Eng
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
